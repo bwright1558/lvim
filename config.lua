edc4f1a8e05f5f9e04c3029afe64e2d57e3006b2
Brian Wright <bw@shadowdragon.io>
2022-03-07 11:40:01 -0600

    don't auto-install yamlls

--- config.lua
+++ config.lua
@@ -94,6 +94,7 @@ lvim.builtin.treesitter.indent.enable = false
 
 -- LSP
 lvim.lsp.diagnostics.virtual_text = false
+vim.list_extend(lvim.lsp.override, { "yamlls" })
 -- lvim.lsp.automatic_servers_installation = false
 
 -- Additional Plugins
