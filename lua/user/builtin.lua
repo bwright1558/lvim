2cb7ee091a57f0f9ed86cee6ae03082511c354a9
Brian Wright <bw@shadowdragon.io>
2022-04-08 12:25:43 -0500

    modular configs

---
+++ lua/user/builtin.lua
@@ -0,0 +1,9 @@
+-- User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.bufferline.active = false
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 1
---
+++ lua/user/options.lua
@@ -0,0 +1,10 @@
+-- General
+lvim.log.level = "warn"
+lvim.format_on_save = false
+lvim.colorscheme = "nightfox"
+vim.opt.cmdheight = 1
+vim.opt.showtabline = 0
+-- vim.opt.timeoutlen = 500
+vim.opt.modeline = false
+vim.opt.relativenumber = true
+vim.opt.whichwrap:remove({ "<", ">", "[", "]", "h", "l" })
