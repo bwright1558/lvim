15b505b7061b1399f9f930f93e0ca4f4e9df3a46
Brian Wright <bw@shadowdragon.io>
2022-04-18 15:58:05 -0500

    fix lsp_signature initialization

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -10,7 +10,7 @@ lvim.plugins = {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function()
-      require("lsp_signature").on_attach()
+      require("lsp_signature").setup()
     end,
   },
   {
