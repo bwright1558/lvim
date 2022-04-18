9c82b2c873ef272079c05af50a06d94da5e9ea0b
Brian Wright <bw@shadowdragon.io>
2022-04-18 09:55:15 -0500

    fix deprecated settings

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,4 +1,4 @@
 -- LSP
 lvim.lsp.diagnostics.virtual_text = false
-vim.list_extend(lvim.lsp.override, { "yamlls" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yamlls" })
 -- lvim.lsp.automatic_servers_installation = false
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,5 +1,5 @@
 -- Treesitter
-lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ensure_installed = "all"
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.indent.enable = false
