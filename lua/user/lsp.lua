7ab4c315a0c05c1a8a5e13b676722c74f323194c
Brian Wright <bw@shadowdragon.io>
2022-04-19 12:56:31 -0500

    finally this works!!!

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -2,3 +2,7 @@
 lvim.lsp.diagnostics.virtual_text = false
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yamlls" })
 -- lvim.lsp.automatic_servers_installation = false
+
+lvim.lsp.on_attach_callback = function(client, bufnr)
+  require("lsp_signature").on_attach()
+end
