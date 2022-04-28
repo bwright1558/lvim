44efe37e0b1b745bd5f1278a7f2430787bec0fd5
Brian Wright <bw@shadowdragon.io>
2022-04-28 10:57:34 -0500

    disable dashboard

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -1,6 +1,6 @@
 -- User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
+lvim.builtin.alpha.active = false
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
