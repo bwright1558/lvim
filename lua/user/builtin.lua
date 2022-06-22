4bdb12ec9776ea74b920c11a6d4a5e3c802c7951
Brian Wright <bw@shadowdragon.io>
2022-06-22 10:04:25 -0500

    fix breaking change with lunarvim config

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -6,5 +6,4 @@ lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.bufferline.active = false
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.project.manual_mode = true
