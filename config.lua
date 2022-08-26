51600ff17912b0cdccb8dbea132ee712defd0dd2
Brian Wright <bw@shadowdragon.io>
2022-08-26 11:04:55 -0500

    don't install yaml language server

--- config.lua
+++ config.lua
@@ -94,7 +94,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.indent.enable = false
 
 -- LSP
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yamlls" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yaml-language-server" })
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.on_attach_callback = function(client, bufnr)
   require("lsp_signature").on_attach()
